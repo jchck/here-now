fc9795ae68151cfb87fe40c68ef25a6fb823fe6c
jaechick <hey@justinchick.com>
2015-07-18 13:36:48 -0500

    register RSS feed

--- here-now.php
+++ here-now.php
@@ -10,3 +10,10 @@ Author URI:         https://justinchick.com/
 License:            MIT License
 License URI:        http://opensource.org/licenses/MIT
 */
+
+namespace jchck\hereNow;
+
+add_action( 'init', 'herenow_pod_rss');
+function herenow_pod_rss(){
+	add_feed( 'here-now', 'here_now_feed' );
+}
