26e04ba144efe9495fbc586596c7749d04e6fb1d
jaechick <hey@justinchick.com>
2015-07-18 16:49:41 -0500

    update rss template for here & now specific use

--- templates/rss.php
+++ templates/rss.php
@@ -4,7 +4,7 @@
 **/
 
 // Query the Podcast Custom Post Type and fetch the latest 100 posts
-$args = array( 'post_type' => 'podcasts', 'posts_per_page' => 100 );
+$args = array( 'post_type' => 'podcast', 'posts_per_page' => 100 );
 $loop = new WP_Query( $args );
 
 // Output the XML header
@@ -20,48 +20,48 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>';
     // Mostly using get_bloginfo() here, but these can be custom tailored, as needed
   ?>
   <channel>
-    <title><?php echo get_bloginfo('name'); ?></title>
+    <title>Here and Now Podcast</title>
     <link><?php echo get_bloginfo('url'); ?></link>
     <language><?php echo get_bloginfo ( 'language' ); ?></language>
     <copyright><?php echo date('Y'); ?> <?php echo get_bloginfo('name'); ?></copyright>
     
     <itunes:author><?php echo get_bloginfo('name'); ?></itunes:author>
-    <itunes:summary><?php echo get_bloginfo('description'); ?></itunes:summary>
-    <description><?php echo get_bloginfo('url'); ?></description>
+    <description>Here and Now is a weekly podcast hosted by Justin Chick and Charles Purnell. Conversations begin with and generally revolve around a verb, what it means to us, and how we relate to it.</description>
+    <itunes:subtitle>Here and Now is a weekly podcast hosted by Justin…</itunes:subtitle>
     
     <itunes:owner>
       <itunes:name><?php echo get_bloginfo('name'); ?></itunes:name>
-      <itunes:email><?php echo get_bloginfo('admin_email'); ?></itunes:email>
+      <itunes:email>hereandnow.pod@gmail.com</itunes:email>
     </itunes:owner>
+
+    <itunes:explicit>yes</itunes:explicit>
     
-    <?php // Change to your own image. Must be at least 1400 x 1400: https://www.apple.com/itunes/podcasts/creatorfaq.html 
-    <itunes:image href="http://your-site.com/path/to/podcast/image.png" />
-    
-    <itunes:category text="Technology">
-      <itunes:category text="Tech News"/>
-    </itunes:category>
+    <?php // Change to your own image. Must be at least 1400 x 1400: https://www.apple.com/itunes/podcasts/creatorfaq.html ?> 
+    <itunes:image href="http://i1.sndcdn.com/avatars-000149273945-enb9kz-original.jpg"/>
+    <image>
+      <url>http://i1.sndcdn.com/avatars-000149273945-enb9kz-original.jpg</url>
+      <title>Here and Now</title>
+      <link>http://hereandnow.io</link>
+    </image>
+    <itunes:category text="Society &amp; Culture"/>
     
     <?php // Start the loop for Podcast posts
     while ( $loop->have_posts() ) : $loop->the_post(); ?>
     <item>
       <title><?php the_title_rss(); ?></title>
       <itunes:author><?php echo get_bloginfo('name'); ?></itunes:author>
-      <itunes:summary><?php the_excerpt_rss(); ?></itunes:summary>
-      <?php // Retrieve just the URL of the Featured Image: http://codex.wordpress.org/Function_Reference/wp_get_attachment_image_src
-      if (has_post_thumbnail( $post->ID ) ): ?>
-        <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' ); ?>
-        <itunes:image href="<?php echo $image[0]; ?>" />
-      <?php endif; ?>
+      <itunes:summary><?php the_excerpt_rss(50, 0); ?></itunes:summary>
+      <itunes:image href="http://i1.sndcdn.com/avatars-000149273945-enb9kz-original.jpg" />
       
       <?php // Get the file field URL and filesize
-        $attachment_id = get_field('podcast_file');
+        $attachment_id = get_field('audio_file');
         $fileurl = wp_get_attachment_url( $attachment_id );
         $filesize = filesize( get_attached_file( $attachment_id ) );
       ?>
       
       <enclosure url="<?php echo $fileurl; ?>" length="<?php echo $filesize; ?>" type="audio/mpeg" />
       <guid><?php echo $fileurl; ?></guid>
-      <guid><?php the_field('podcast_file'); ?></guid>
+      <guid><?php the_field('audio_file'); ?></guid>
       <pubDate><?php the_time( 'D, d M Y G:i:s T') ?></pubDate>
       <itunes:duration><?php the_field('podcast_duration'); ?></itunes:duration>
     </item>
