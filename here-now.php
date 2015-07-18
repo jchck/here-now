dd71c3c11344a824c91badc5172ae3370e90c525
jaechick <hey@justinchick.com>
2015-07-18 13:29:22 -0500

    init the plugin

---
+++ here-now.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name:        Here & Now
+Plugin URI:         https://hereandnow.io/
+Description:        A pplugin to host and manage your podcast via WordPress
+Version:            0.0.0
+Author:             jchck_
+Author URI:         https://justinchick.com/
+License:            MIT License
+License URI:        http://opensource.org/licenses/MIT
+*/
